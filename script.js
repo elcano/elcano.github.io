991588e4faeaf066c6cdecd3e7ca12a70fe812c1
AbbyHoyt <abby.hoyt@comcast.net>
2026-09-06 16:42:40 -0700

    Fixed mobile navbar so you don't need to double-click to open the menu the first time.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 /*Adjust Navbar Depending on Screen Size*/
+var firstToggle = true;
+
 function adjustNavBar() {
     var x = document.getElementById("global-navbar");
-    if (x.className === "navbar") {
-        x.className += " responsive";
+
+    if (firstToggle == true) {
+        if (x.className === "navbar") {
+            x.className = "navbar";
+        } else {
+            x.className += " responsive";
+        }
+
+        firstToggle = false;
     } else {
-        x.className = "navbar";
+        if (x.className === "navbar") {
+            x.className += " responsive";
+        } else {
+            x.className = "navbar";
+        }
     }
 }
